df870c379e564bc1ab951d2325070bdf39ebfdea
Bortoleto <ericbortoleto_9@outlook.com>
2026-08-13 19:44:41 -0300

    setup: inicializa o projeto F1TeamApp com Jetpack Compose
    - Adiciona a estrutura base do projeto utilizando Gradle Kotlin DSL e Version Catalog.
    - Configura o Jetpack Compose com suporte a Material3 e temas customizados.
    - Implementa a `MainActivity` básica com suporte a edge-to-edge e componentes de UI iniciais.
    - Adiciona recursos essenciais como ícones adaptativos, strings e definições de cores.
    - Configura os arquivos de build, scripts do Gradle Wrapper e arquivos `.gitignore`.
    - Inclui templates iniciais para testes unitários e instrumentados.

---
+++ app/src/main/java/io/github/bortoletoeric/f1teamapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package io.github.bortoletoeric.f1teamapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.bortoletoeric.f1teamapp.ui.theme.F1TeamAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            F1TeamAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    F1TeamAppTheme {
+        Greeting("Android")
+    }
+}
